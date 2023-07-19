ee3131edac3545fc3b01f156741613081b353bea
Mariano García Benito <mdgarciabenito@gmail.com>
2023-07-19 18:21:22 -0300

    second commit

--- js/app.js
+++ js/app.js
@@ -1,39 +1,32 @@
-let ronda = 0;
-let golesArgentina = 0;
-let golesFrancia = 0;
-let resultadoArgentina = 0
-let resultadoFrancia = 0
-const ambosJuegan = ()=> resultadoArgentina<=3 || resultadoFrancia<=3;
+let golesEquipo1 = 0;
+let golesEquipo2 = 0;
+let resultadoEquipo1 = 0;
+let resultadoEquipo2 = 0;
+function nombreEquipo1(){
+    equipo1 = prompt("Ingresar el nombre del primer equipo")
+    alert("El primer equipo es " + equipo1)
+}
+function nombreEquipo2(){
+    equipo2 = prompt("Ingresar el nombre del segundo equipo")
+    alert("El segundo equipo es " + equipo2)
+}
+nombreEquipo1()
+nombreEquipo2()
 
-/*let pateaArgentina = Math.round(Math.random()*1);
-if(pateaArgentina == 1)
-    console.log ("Golaaaazo de Argentinaaaaaaaaaa");
-if(pateaArgentina == 0)
-    console.log ("El tiro se va afueraaaaaa, Argentina tiembla");
-
-let pateaFrancia = Math.round(Math.random()*1);
-if(pateaFrancia == 1)
-    console.log ("Gol de Francia, me quiero morir");
-if(pateaFrancia == 0)
-    console.log ("El tiro se va afueraaaaaa, Argentina festeja");
-resultadoArgentina = resultadoArgentina + pateaArgentina;
-resultadoFrancia =  resultadoFrancia + pateaFrancia
-console.log("Argentina " + resultadoArgentina + " / Francia " + resultadoFrancia)*/
-
-while(resultadoArgentina<=7 || resultadoFrancia<=7){
-    let pateaArgentina = Math.round(Math.random()*1);
-    if(pateaArgentina == 1)
-        console.log ("Golaaaazo de Argentinaaaaaaaaaa");
-    if(pateaArgentina == 0)
-        console.log ("El tiro se va afueraaaaaa, Argentina tiembla");
+while(resultadoEquipo1<7 && resultadoEquipo2<7){
+    let pateaEquipo1 = Math.round(Math.random()*1);
+    if(pateaEquipo1 == 1)
+        console.log ("Golaaaazo de " + equipo1 + ". La tribuna explota");
+    if(pateaEquipo1 == 0)
+        console.log (equipo1 + " con un tiro errado, se va afueraaaaaa!");
     
-    let pateaFrancia = Math.round(Math.random()*1);
-    if(pateaFrancia == 1)
-        console.log ("Gol de Francia, me quiero morir");
-    if(pateaFrancia == 0)
-        console.log ("El tiro se va afueraaaaaa, Argentina festeja");
-    resultadoArgentina = resultadoArgentina + pateaArgentina;
-    resultadoFrancia =  resultadoFrancia + pateaFrancia
-    console.log("Argentina " + resultadoArgentina + " / Francia " + resultadoFrancia)
+    let pateaEquipo2 = Math.round(Math.random()*1);
+    if(pateaEquipo2 == 1)
+        console.log ("Gol de " + equipo2 + " toda la hinchada aplaude");
+    if(pateaEquipo2 == 0)
+        console.log (equipo2 + " erra el tiro!! Nadie lo puede creer.")
+    resultadoEquipo1 = resultadoEquipo1 + pateaEquipo1
+    resultadoEquipo2 =  resultadoEquipo2 + pateaEquipo2
+    console.log( equipo1 + " " + resultadoEquipo1 + " / " + equipo2 + " " + resultadoEquipo2);
 }
 
